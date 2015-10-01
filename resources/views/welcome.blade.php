777eb9daf2835cbb9c8f2eea4d18da81688e9898
pobleteallen <pobleteallen@gmail.com>
2015-10-01 18:46:48 +0800

    Additional segment
    addition about us info

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,21 +10,41 @@
        
     </head>
     <body>
-        <div id="header-bg" class="ui vertical segment">
+        <div id="header-bg" class="ui inverted vertical segment">
             <div class="ui container">
                 <div class="ui inverted large secondary menu">
                     <div class="header item">
                         apstudio
                     </div>
                     <div class="ui right secondary menu">
-                        <a class="item">Home</a>
+                        <a class="active item">Home</a>
                         <a class="item">Mission</a>
                         <a class="item">Services</a>
-                        <a class="item">About Us</a>
-                        <a class="item">Contact Us</a>
+                        <a class="item">About us</a>
+                        <a class="item">Contact us</a>
                     </div>
                 </div>
             </div>
-        <div>
+
+            <div class="ui container">
+                <h1 class="ui inverted right floated header">
+                    <div class="left aligned sub header">A wide range of security services for your business.</div>
+                    It's an honour for us to serve you  
+                </h1>
+            </div>
+        </div>
+
+        <div id="aboutus" class="ui vertical segment">
+            <div class="ui container">
+                <h2 class="ui icon center aligned header">
+                    <i class="users icon"></i>
+                    <div class="content">
+                        About us
+                        <div class="sub header">Security services you can trust</div>
+                    </div>
+                </h2>
+
+            </div>
+        </div>
     </body>
 </html>
