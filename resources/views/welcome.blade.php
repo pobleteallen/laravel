a6b9ab0d465a5c1401fd82d2f292463958f0f57e
pobleteallen <pobleteallen@gmail.com>
2015-10-01 20:01:29 +0800

    about us
    putting icons each column

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,7 @@
                         <a class="active item">Home</a>
                         <a class="item">Mission</a>
                         <a class="item">Services</a>
-                        <a class="item">About us</a>
+                        <a href="#aboutus" class="item">About us</a>
                         <a class="item">Contact us</a>
                     </div>
                 </div>
@@ -35,16 +35,65 @@
         </div>
 
         <div id="aboutus" class="ui vertical segment">
-            <div class="ui container">
-                <h2 class="ui icon center aligned header">
+                
+            <div class="ui text container">
+                <h1 class="ui header">
                     <i class="users icon"></i>
                     <div class="content">
                         About us
                         <div class="sub header">Security services you can trust</div>
                     </div>
-                </h2>
+                </h1>
+                <p>Our Company's security assessments leverage our extensive knowledge of current security vulnerabilities,
+                    penetration techniques and software development best practices to enable customers to secure their systems
+                     against ever-present threats.
+                </p>
+            </div>
+            <div class="ui hidden divider"></div>
+            <div class="ui four column grid container">
+        
+                <div class="column">
+                    <h1 class="ui icon center aligned header">
+                        <i class="massive idea icon"></i>
+                        <div class="content">Smarter</div>
+                    </h1>
+                    <p align="center">Our Company's security assessments leverage our extensive knowledge 
+                        of current security vulnerabilities, penetration
+                    </p>
+                </div>
+
+                <div class="column">
+                    <h1 class="ui icon center aligned header">
+                        <i class="massive wait icon"></i>
+                        <div class="content">Faster</div>
+                    </h1>
+                    <p align="center">Our Company's security assessments leverage our extensive knowledge 
+                        of current security vulnerabilities, penetration
+                    </p>
+                </div>
+
+                <div class="column">
+                    <h1 class="ui icon center aligned header">
+                        <i class="massive linkify icon"></i>
+                        <div class="content">Stronger</div>
+                    </h1>
+                    <p align="center">Our Company's security assessments leverage our extensive knowledge 
+                        of current security vulnerabilities, penetration
+                    </p>
+                </div>
+
+                <div class="column">
+                    <h1 class="ui icon center aligned header">
+                        <i class="massive thumbs up icon"></i>
+                        <div class="content">Better</div>
+                    </h1>
+                    <p align="center">Our Company's security assessments leverage our extensive knowledge 
+                        of current security vulnerabilities, penetration
+                    </p>
+                </div>
 
             </div>
+                
         </div>
     </body>
 </html>
