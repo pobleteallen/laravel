14cd4ce4b6fe1f89a2261a1bccdddb1533b9d5cf
pobleteallen <pobleteallen@gmail.com>
2015-10-08 13:56:31 +0800

    1

--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,83 @@
 $(document).ready(function(){
 
 	// scroll to top code
+	// $(window).scroll(function(){
+	// 	if($(window).scrollTop() > 0)
+	// 	{
+	// 		$('#scrolltotop').fadeIn('fast');
+	// 		// alert('more');
+	// 	}
+	// 	else
+	// 	{
+	// 		$('#scrolltotop').fadeOut('fast');
+	// 	}
+	// });
+	// $('#scrolltotop').click(function(){
+	// 	$('html,body').animate({scrollTop : 0},'slow')
+	// });
+
+	//after user passed the header div fixed menu will appear
 	$(window).scroll(function(){
-		if($(window).scrollTop() > 0)
+		if($(window).scrollTop() == 0)
 		{
-			$('#scrolltotop').fadeIn('fast');
-			// alert('more');
+			$('body').css('overflow','hidden');
 		}
 		else
 		{
-			$('#scrolltotop').fadeOut('fast');
+			$('body').css('overflow','auto');
+		}
+		
+		if($(window).scrollTop() > 0)
+		{
+			if($(window).width() > 700)
+			{
+				$('#fixed-menu').removeClass('secondary')
+				$('#fixed-menu .item:gt(0)').css('display','inline-block');
+			}
+			
+		}
+		else
+		{
+			$('#fixed-menu').addClass('secondary')
+			$('#fixed-menu .item:gt(0)').css('display','none');
 		}
 	});
-	$('#scrolltotop').click(function(){
-		$('html,body').animate({scrollTop : 0},'slow')
+
+	//everytime the viewport change this will trigger
+	$(window).resize(function(){
+		//when ever the viewport is changing style of menu change
+		if($(window).width() > 700)
+		{
+			if($(window).scrollTop() > $('#header')[0].scrollHeight)
+			{
+				$('#fixed-menu').removeClass('secondary')
+				$('#fixed-menu .item:gt(0)').css('display','inline-block');
+			}
+			
+		}
+		else
+		{
+			$('#fixed-menu').addClass('secondary')
+			$('#fixed-menu .item:gt(0)').css('display','none');
+		}
 	});
 
+
+	//slide to navigate site
+	$('#navigate-site').click(function(){
+		$('html,body').animate({ scrollTop: $('#header')[0].scrollHeight },'slow', function(){
+			$('body').css('overflow','auto');
+		});
+		
+	});
+
+	//menu internal link transition
 	$('.menu .item').click(function(){
 		$('html,body').animate({scrollTop : $($(this).attr('href')).offset().top},'slow');
 		return false;
 	});
 
-	//header slide
+	//header quote animate
 	$('#header .ui.header span li:gt(0)').hide();
 	setInterval(function(){
 		$('#header .ui.header span li:first')
@@ -44,8 +100,9 @@ $(document).ready(function(){
 	}, 5000);
 
 	//mobile menu
-	$('#mainmenu').on('click','.mobile.item',function(){
-		$('.ui.sidebar').sidebar('show');
+	//$('.ui.sidebar').sidebar({overlay: true, scrollLock: true},'attach events','.mobile.item');
+	$('#fixed-menu').on('click','.mobile.item',function(){
+		$('.ui.sidebar').sidebar({overlay: true, scrollLock: true}).sidebar('show');
 	});
 	
 	
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <!-- Standard Meta -->
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+
         <title>Laravel</title>
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
@@ -14,170 +20,177 @@
        
     </head>
     <body>
-        <!-- mobile menu -->
-        <div class="ui vertical inverted sidebar menu">
-            <a href="#services" class="item">Services</a>
-            <a href="#feedbacks" class="item">Feedbacks</a>
-            <a href="#aboutus" class="item">About us</a>
-            <a href="#contactus" class="item">Contact us</a>
-        </div>
-        <!-- scrolltotop icon -->
-        <!-- <i id="scrolltotop" class="link big bordered inverted black chevron up icon" 
-        style="position: fixed; bottom: 40px; right: 20px; display: none; z-index: 999999999999;"></i> -->
-        <!--  -->
-        <div id="header" class="ui inverted vertical segment">
-            <div class="ui container">
-                <div class="ui vertical segment">
-                    <div id="mainmenu" class="ui inverted large secondary menu">
-                        <div class="ui large right secondary menu">      
-                            <a href="#services" class="item">Services</a>
-                            <a href="#feedbacks" class="item">Feedbacks</a>
-                            <a href="#aboutus" class="item">About us</a>
-                            <a href="#contactus" class="item">Contact us</a>
-                            <a class="mobile item"><i class="sidebar large icon"></i></a>
-                        </div>
-                    </div>
-                </div>
+        <div>
+            <!-- mobile menu -->
+            <div class="ui vertical very thin inverted sidebar menu">
+                <a href="#header" class="item">Home</a>
+                <a href="#services" class="item">Services</a>
+                <a href="#feedbacks" class="item">Feedbacks</a>
+                <a href="#aboutus" class="item">About us</a>
+                <a href="#contactus" class="item">Contact us</a>
+            </div>
 
-                <div class="ui vertical segment">
-                    <!-- <img id="logo" class="ui centered small image" src="img/gap-logo.png"> -->
-                    <h3 class="ui inverted centered header">
-                        SECURITY SERVICES<br><span style="margin-left: -8.5rem">FOR YOUR&nbsp<li>BUSINESS</li><li>BUILDING</li><li>HOME</li></span>
-                    </h3>
+            <!-- fixed menu -->
+            <div id="fixed-menu" class="ui inverted large secondary fixed menu"> 
+                <div class="ui container">
+                    <a class="mobile item"><i class="circular inverted large sidebar icon"></i></a> 
+                    <div class="item header"><img src="img/gap-logo.png"></div>
+                    <a href="#header" class="item">Home</a>
+                    <a href="#services" class="item">Services</a>
+                    <a href="#feedbacks" class="item">Feedbacks</a>
+                    <a href="#aboutus" class="item">About us</a>
+                    <a href="#contactus" class="item">Contact us</a>  
                 </div>
             </div>
-        </div>
 
-        <div id="services" class="ui vertical segment">
-           <h2 class="ui text container horizontal divider">our services</h2>
-            <div class="ui stackable three column grid container">               
-                <div class="column">
-                    <h4 class="ui icon center aligned header">
-                        <i class="massive circular inverted blue home icon"></i>
-                        <div class="ui hidden divider"></div>
-                        <div class="content">HOME SECURITY</div>
-                    </h4>
-                    <p>Dolore cuml dia sed in lacus ut eniascet inger es sitet amet
-                     eismod ictor ut ligulate ameti dapib ticdu nt mtsent dolor lte comme.Dolore cuml dia sed in lacus ut eniascet inger es sitet amet
-                     eismod ictor ut ligulate ameti dapib ticdu nt mtsent dolor lte comme
-                    </p>
-                </div>
-
-                <div class="column">
-                    <h4 class="ui icon center aligned header">
-                        <i class="massive circular inverted blue briefcase icon"></i>
-                        <div class="ui hidden divider"></div>
-                        <div class="content">BUSINESS SECURITY</div>
-                    </h4>
-                    <p>Dolore cuml dia sed in lacus ut eniascet inger es sitet amet
-                     eismod ictor ut ligulate ameti dapib ticdu nt mtsent dolor lte comme.Dolore cuml dia sed in lacus ut eniascet inger es sitet amet
-                     eismod ictor ut ligulate ameti dapib ticdu nt mtsent dolor lte comme
-                    </p>
-                </div>
-
-                <div class="column">
-                    <h4 class="ui icon center aligned header">
-                        <i class="massive circular inverted blue building icon"></i>
-                        <div class="ui hidden divider"></div>
-                        <div class="content">BUILDING SECURITY</div>
-                    </h4>
-                    <p>Dolore cuml dia sed in lacus ut eniascet inger es sitet amet
-                     eismod ictor ut ligulate ameti dapib ticdu nt mtsent dolor lte comme.Dolore cuml dia sed in lacus ut eniascet inger es sitet amet
-                     eismod ictor ut ligulate ameti dapib ticdu nt mtsent dolor lte comme
-                    </p>
-                </div>
-            </div> 
-        </div>
-
-        <div id="feedbacks" class="ui inverted vertical segment">  
-            <div class="ui text container">
-                <h2 class="ui inverted horizontal divider">feedback</h2>
-                <div class="testimonial-holder">
-                    <div id="testimonial" class="ui testi vertical segment">
-                        <h3 class="ui inverted icon center aligned header">   
-                            <img class="ui small circular image" src="img/testi-pic.jpg">
-                            <div class="content">
-                                Allen Poblete
-                                <div class="sub header">Mall Director</div>
-                            </div>
+            <!-- HEADER/BANNER/MENU -->
+            <div id="header" class="ui inverted vertical segment">
+                <!-- navigate site button  -->
+                <i id="navigate-site" class="link huge inverted angle double down icon" 
+                style="position: absolute; bottom: 1%; left: 50%; transform: translate(-50%, -1%); z-index: 5;"></i>
+                <div class="ui container">
+                    <div class="ui vertical segment">
+                        <img id="logo" class="ui centered small image" src="img/gap-logo.png">
+                        <h3 class="ui inverted centered header">
+                            SECURITY SERVICES<br><span style="margin-left: -8.5rem">FOR YOUR&nbsp<li>BUSINESS</li><li>BUILDING</li><li>HOME</li></span>
                         </h3>
-                        <p style="color: white">
-                            <i class="inverted quote left icon"></i>1Dolore cuml dia sed in lacus ut eniascet inger es sitet amet
-                             eismod ictor ut ligulate ameti dapib ticdu nt mtsent dolor lte comme. Mes cumldi sed inertio. 
-                             Lacus ut eniascet ingerto aliiqt sit amet eism odictor ipsum commete.<i class="inverted quote right icon"></i>
+                    </div>
+                </div>
+            </div>
+
+            <!-- OUR SERVICES -->
+            <div id="services" class="ui vertical segment">
+               <h2 class="ui text container horizontal divider">our services</h2>
+               <div class="ui hidden divider"></div>
+                <div class="ui stackable three column grid container">               
+                    <div class="column">
+                        <h4 class="ui icon center aligned header">
+                            <i class="massive circular inverted blue home icon"></i>
+                            <div class="ui hidden divider"></div>
+                            <div class="content">HOME SECURITY</div>
+                        </h4>
+                        <p>Dolore cuml dia sed in lacus ut eniascet inger es sitet amet
+                         eismod ictor ut ligulate ameti dapib ticdu nt mtsent dolor lte comme.Dolore cuml dia sed in lacus ut eniascet inger es sitet amet
+                         eismod ictor ut ligulate ameti dapib ticdu nt mtsent dolor lte comme
                         </p>
                     </div>
 
-                    <div id="testimonial" class="ui testi vertical segment">
-                        <h3 class="ui inverted icon center aligned header">   
-                            <img class="ui small circular image" src="img/testi-pic.jpg">
-                            <div class="content">
-                                Allen Poblete
-                                <div class="sub header">Mall Director</div>
-                            </div>
-                        </h3>
-                        <p style="color: white">
-                            <i class="inverted quote left icon"></i>Maecenas fermentum nulla purus, ac commodo ipsum pretium at. 
-                            Etiam vitae luctus metus. Mauris nunc nisl, elementum eu molestie eget, ultricies sit amet libero. 
-                            Suspendisse et tempus odio. Morbi vel aliquam nulla, tristique laoreet risus.<i class="inverted quote right icon"></i>
+                    <div class="column">
+                        <h4 class="ui icon center aligned header">
+                            <i class="massive circular inverted blue briefcase icon"></i>
+                            <div class="ui hidden divider"></div>
+                            <div class="content">BUSINESS SECURITY</div>
+                        </h4>
+                        <p>Dolore cuml dia sed in lacus ut eniascet inger es sitet amet
+                         eismod ictor ut ligulate ameti dapib ticdu nt mtsent dolor lte comme.Dolore cuml dia sed in lacus ut eniascet inger es sitet amet
+                         eismod ictor ut ligulate ameti dapib ticdu nt mtsent dolor lte comme
                         </p>
                     </div>
-                </div>
 
+                    <div class="column">
+                        <h4 class="ui icon center aligned header">
+                            <i class="massive circular inverted blue building icon"></i>
+                            <div class="ui hidden divider"></div>
+                            <div class="content">BUILDING SECURITY</div>
+                        </h4>
+                        <p>Dolore cuml dia sed in lacus ut eniascet inger es sitet amet
+                         eismod ictor ut ligulate ameti dapib ticdu nt mtsent dolor lte comme.Dolore cuml dia sed in lacus ut eniascet inger es sitet amet
+                         eismod ictor ut ligulate ameti dapib ticdu nt mtsent dolor lte comme
+                        </p>
+                    </div>
+                </div> 
             </div>
-        </div>
 
-        <div id="aboutus" class="ui vertical segment">
-                
-            <div class="ui text container">
-                <h2 class="ui horizontal divider">about us</h2>
-                <div class="ui hidden divider"></div>
-                <h2 class="ui icon center aligned header">
-                    <i class="massive circular inverted blue users icon"></i>
-                </h2>
-                <p align="center">Dolore cuml dia sed in lacus ut eniascet inger es sitet amet
-                 eismod ictor ut ligulate ameti dapib ticdu nt mtsent dolor lte comme. Mes cumldi sed inertio. 
-                 Lacus ut eniascet ingerto aliiqt sit amet eism odictor ipsum commete.
-                </p>
-            </div>
-        </div>
-
-        <div id="contactus" class="ui inverted vertical segment">
-
-            <div class="ui text container">
-                <h2 class="ui inverted horizontal divider">Contact Us</h2>
-                <div class="ui hidden divider"></div>
-                <div class="ui hidden divider"></div>
-                <div class="ui hidden divider"></div>
-                <p>Dolore cuml dia sed in lacus ut eniascet inger es sitet amet
-                     eismod ictor ut ligulate ameti dapib ticdu nt mtsent dolor lte comme.Dolore cuml dia sed in lacus ut eniascet inger es sitet amet
-                     eismod ictor ut ligulate ameti dapib ticdu nt mtsent dolor lte comme
-                </p>
-                <div class="ui hidden divider"></div>
-                <form class="ui large form">
-                    <div class="two fields">
-                        <div class="grouped fields">
-                            <div class="field">
-                                <input type="text" name="name" placeholder="Full Name">
-                            </div>
-                            <div class="field">
-                                <input type="text" name="name" placeholder="Email Address">
-                            </div>
-                            <div class="field">
-                                <input type="text" name="name" placeholder="Phone Number">
-                            </div>
+            <!-- FEEDBACKS -->
+            <div id="feedbacks" class="ui inverted vertical segment">  
+                <div class="ui text container">
+                    <h2 class="ui inverted horizontal divider">feedback</h2>
+                    <div class="testimonial-holder">
+                        <div id="testimonial" class="ui testi vertical segment">
+                            <h3 class="ui inverted icon center aligned header">   
+                                <img class="ui small circular image" src="img/testi-pic.jpg">
+                                <div class="content">
+                                    Allen Poblete
+                                    <div class="sub header">Mall Director</div>
+                                </div>
+                            </h3>
+                            <p style="color: white">
+                                <i class="inverted quote left icon"></i>1Dolore cuml dia sed in lacus ut eniascet inger es sitet amet
+                                 eismod ictor ut ligulate ameti dapib ticdu nt mtsent dolor lte comme. Mes cumldi sed inertio. 
+                                 Lacus ut eniascet ingerto aliiqt sit amet eism odictor ipsum commete.<i class="inverted quote right icon"></i>
+                            </p>
                         </div>
-                        <div class="field">
-                            <textarea style="margin-top: .4rem;" rows="6" placeholder="Message"></textarea>
+
+                        <div id="testimonial" class="ui testi vertical segment">
+                            <h3 class="ui inverted icon center aligned header">   
+                                <img class="ui small circular image" src="img/testi-pic.jpg">
+                                <div class="content">
+                                    Allen Poblete
+                                    <div class="sub header">Mall Director</div>
+                                </div>
+                            </h3>
+                            <p style="color: white">
+                                <i class="inverted quote left icon"></i>Maecenas fermentum nulla purus, ac commodo ipsum pretium at. 
+                                Etiam vitae luctus metus. Mauris nunc nisl, elementum eu molestie eget, ultricies sit amet libero. 
+                                Suspendisse et tempus odio. Morbi vel aliquam nulla, tristique laoreet risus.<i class="inverted quote right icon"></i>
+                            </p>
                         </div>
                     </div>
+
+                </div>
+            </div>
+
+            <!-- ABOUT US -->
+            <div id="aboutus" class="ui vertical segment">
                     
-                </form>
+                <div class="ui text container">
+                    <h2 class="ui horizontal divider">about us</h2>
+                    <div class="ui hidden divider"></div>
+                    <h2 class="ui icon center aligned header">
+                        <i class="massive circular inverted blue users icon"></i>
+                    </h2>
+                    <p align="center">Dolore cuml dia sed in lacus ut eniascet inger es sitet amet
+                     eismod ictor ut ligulate ameti dapib ticdu nt mtsent dolor lte comme. Mes cumldi sed inertio. 
+                     Lacus ut eniascet ingerto aliiqt sit amet eism odictor ipsum commete.
+                    </p>
+                </div>
             </div>
-        </div>
 
-        <div class="ui inverted vertical segment">
+            <!-- CONTACT US -->
+            <div id="contactus" class="ui inverted vertical segment">
 
-        </div>
+                <div class="ui text container">
+                    <h2 class="ui inverted horizontal divider">Contact Us</h2>
+                    <div class="ui hidden divider"></div>
+                    <div class="ui hidden divider"></div>
+                    <div class="ui hidden divider"></div>
+                    <p>Dolore cuml dia sed in lacus ut eniascet inger es sitet amet
+                         eismod ictor ut ligulate ameti dapib ticdu nt mtsent dolor lte comme.Dolore cuml dia sed in lacus ut eniascet inger es sitet amet
+                         eismod ictor ut ligulate ameti dapib ticdu nt mtsent dolor lte comme
+                    </p>
+                    <div class="ui hidden divider"></div>
+                    <form class="ui large form">
+                        <div class="two fields">
+                            <div class="grouped fields">
+                                <div class="field">
+                                    <input type="text" name="name" placeholder="Full Name">
+                                </div>
+                                <div class="field">
+                                    <input type="text" name="name" placeholder="Email Address">
+                                </div>
+                                <div class="field">
+                                    <input type="text" name="name" placeholder="Phone Number">
+                                </div>
+                            </div>
+                            <div class="field">
+                                <textarea style="margin-top: .4rem;" rows="6" placeholder="Message"></textarea>
+                            </div>
+                        </div>
+                        
+                    </form>
+                </div>
+            </div>
+
+        </div><!-- end of pusher -->
     </body>
 </html>
